938b42978a918e8e945f38210b39ddaa2f11c2ac
cedric05 <cedric05@users.noreply.github.com>
2022-09-13 16:06:23 +0000

    deploy: 20b2bb9a3b82a13dc56b64622e2df26786d13759

--- dothttp.worker.bundle.js
+++ dothttp.worker.bundle.js
@@ -4782,7 +4782,7 @@ function _loadPyodideAndPackages() {
 
           case 4:
             _context2.next = 6;
-            return self.pyodide.runPython("import micropip;micropip.install(['dothttp-req-wasm==0.0.41a7', 'setuptools'])");
+            return self.pyodide.runPython("import micropip;micropip.install(['dothttp-req-wasm==0.0.41', 'setuptools'])");
 
           case 6:
             _context2.next = 8;
