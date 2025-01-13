1423aa8c3bd575c6adbbbde3716c85accbde6cab
Prasanth <kesavarapu.siva@gmail.com>
2025-01-13 18:08:46 +0530

    Update worker.ts

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -83,7 +83,7 @@ async function loadPyodideAndPackages() {
     // @ts-ignore
     await self.pyodide.loadPackage(['micropip']);
     // @ts-ignore
-    await self.pyodide.runPython(`import micropip;micropip.install(['dothttp-req-wasm==0.0.43a1', 'setuptools', 'textx'])`);
+    await self.pyodide.runPython(`import micropip;micropip.install(['dothttp-req-wasm==0.0.43', 'setuptools', 'textx'])`);
     // @ts-ignore
     await self.pyodide.runPython(loadCode);
     self.postMessage(
